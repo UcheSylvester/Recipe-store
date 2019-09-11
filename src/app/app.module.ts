f14be2be5a020804a69b5b655343313f5b657deb
UcheSylvester <okorocode@gmail.com>
2019-09-11 19:44:36 +0100

    creating project components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ShoppingList } from "./shopping-list/shopping-list.component";
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { EditShoppingListComponent } from './edit-shopping-list/edit-shopping-list.component';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-item/recipe-detail/recipe-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ShoppingList, RecipeListComponent, EditShoppingListComponent, RecipeItemComponent, RecipeDetailComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
