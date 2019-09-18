91f2fdec46deb9f53aa429a0b9ae7a664abb2fb9
UcheSylvester <okorocode@gmail.com>
2019-09-18 06:41:06 +0100

    working on shopping-list using localRef

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  ViewChild,
+  ElementRef,
+  Output
+} from "@angular/core";
+import { Ingredient } from "src/app/shared/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  selector: "app-shopping-edit",
+  templateUrl: "./shopping-edit.component.html",
+  styleUrls: ["./shopping-edit.component.css"]
 })
 export class ShoppingEditComponent implements OnInit {
+  // @ViewChild("nameInput") nameInputRef: ElementRef;
+  // @ViewChild("amountInput") amountInputRef: ElementRef;
 
-  constructor() { }
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onAddItem(nameInput: HTMLInputElement, amountInput: HTMLInputElement) {
+    const ingName = nameInput.value;
+    const ingAmount = +amountInput.value;
+
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
   }
-
 }
