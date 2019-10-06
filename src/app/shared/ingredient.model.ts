71379048bb738d9c566e8377ffc15f38c6e34756
UcheSylvester <okorocode@gmail.com>
2019-10-06 22:51:52 +0100

    creating services

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -4,3 +4,8 @@ export class Ingredient {
 
   constructor(public name: string, public amount: number) {}
 }
+
+// export interface Ingredient {
+//   name: string;
+//   amount: number;
+// }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from "@angular/core";
-
+import { Injectable, EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 @Injectable()
-export class ShoppingListComponent {}
+export class ShoppingListService {
+  private ingredients: Ingredient[] = [
+    new Ingredient("peper", 10),
+    new Ingredient("tomatoes", 40)
+  ];
+
+  ingredientAdded = new EventEmitter<Ingredient>();
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  onAddItem(nameInput: HTMLInputElement, amountInput: HTMLInputElement) {
+    const ingName = nameInput.value;
+    const ingAmount = +amountInput.value;
+
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
+}
